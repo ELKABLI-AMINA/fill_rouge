c6dc783d8e448586797df6209b8ef5b5d175615d
Amina <amina.elkabli1@gmail.com>
2023-03-04 11:44:45 +0100

    style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,31 +2,65 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Travel</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- fontawesome icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+    <!-- google fonts -->
+    <link rel="stylesheet" href="link rel="preconnect href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo&display=swap" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-    <a class="navbar-brand" href="index-2.html">Backseat Surfing</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="oi oi-menu"></span> Menu
-    </button>
-    <div class="collapse navbar-collapse" id="ftco-nav">
-    <ul class="navbar-nav ml-auto">
-    <li class="nav-item active"><a href="index-2.html" class="nav-link">Home</a></li>
-    <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-    <li class="nav-item"><a href="destination.html" class="nav-link">Destination</a></li>
-    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-    <li class="nav-item cta"><a href="#" class="nav-link">Book Now</a></li>
-    </ul>
+    <!-- navbar -->
+
+    <!-- Section -->
+    <div class="container-fluid position-relative p-0">
+        <div class="container-fluid bg-primary py-5 mb-5 hero-header">
+           <div class="container py-5">
+                <div class="row justify-content-center py-5">
+                  <div class="  col-lg-10 pt-lg-5 mt-lg-5 text-center">
+                        <h1 class="  display-3 text-white mb-3">Enjoy Your Vacation With Us</h1>
+                        <P class="fs-4 text-white mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sapiente, qui quibusdam molestiae ad, aut ipsam inventore praesentium temporibus explicabo illum officia, nam cum totam neque ab quidem natus voluptates!</P>
+                        <div class="position-relative w-75 mx-auto">
+                            <!-- input box -->
+                            <input type="text" class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5 " placeholder="Eg:Tanger">
+                            <button type="button" class="btn btn-primary rounded-pill py-2 px-4 position-absolute top-0 end-0 me-2" style="margin-top:7px;">Search</button>
+
+
+                        </div>
+                  </div>
+                </div>
+             </div>
+        </div>
     </div>
+    <!-- about section -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6" style="min-height: 400px;">
+                 <div class="position-relative h-100">
+                    <img src="" alt="" class="img-fluid position-absolute w-100 h-100">
+
+                 </div>   
+
+                </div>
+
+            </div>
+        </div>
     </div>
-    </nav>
-    
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>  
+    <script src="main.js"></script>
 </body>
 </html>
