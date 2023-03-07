2cae60f126799db88d5cbb2afb945c30925be879
Amina <amina.elkabli1@gmail.com>
2023-03-07 16:48:51 +0100

    package-1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,21 +91,21 @@
                     <div class="row g-3">
                         <div class="col-lg-12 col-md-12">
                             <a href="" class="d-block position-relative overflow-hidden">
-                                <img src="{{asset('img/kech.jpg')}}" alt="">
+                                <img src="{{asset('img/about.jpg')}}" alt="" class="fluid">
                                 <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">30% OFF </div>
                                 <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">MARRAKECH </div>
                             </a>
                         </div>
-                        {{-- <div class="col-lg-6 col-md-12">
+                        <div class="col-lg-6 col-md-12">
                             <a href="" class="d-block position-relative overflow-hidden">
-                                <img src="{{asset('img/Akchour.jpg')}}" alt="">
+                                <img src="{{asset('img/Akchour.jpg')}}" alt=""  class="fluid">
                                 <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">14% OFF </div>
                                 <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">AKCHOUR </div>
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-12">
                             <a href="" class="d-block position-relative overflow-hidden">
-                                <img src="{{asset('img/Merzouga.jpg')}}" alt="">
+                                <img src="{{asset('img/Merzouga.jpg')}}" alt=""  class="fluid">
                                 <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">14% OFF </div>
                                 <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">MERZOUGA </div>
                             </a>
@@ -119,7 +119,37 @@
                     <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">20% OFF</div>
                     <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">Tanger</div>
                 </a></div>
-            </div> --}}
+            </div>
+        </div>
+    </div>
+    {{-- package --}}
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center">
+                <h6 class="section-title bg-white text-center text-primary px-3">Packages</h6>
+                <h1 class="mb-5">Awesome Packages</h1>
+            </div>
+            <div class="row g-4 justify-content-center">
+                <div class="col-lg-4 col-md-6">
+                   <div class=" .package-item">
+                    <div class="overflow-hidden">
+                        <img src="{{asset('img/tanger.jpg')}}" alt="" class="img-fluid">
+                    </div>
+                      <div class="d-flex border-bottom">
+                         <small class="flex-fill text-center border-end py-2">
+                            <i class="fa fa-map-marker-alt text-primary me-2"></i> Tanger
+                         </small>
+                         <small class="flex-fill text-center border-end py-2">
+                            <i class="fa fa-map-calender text-primary me-2"></i> 3 days
+                         </small>
+                         <small class="flex-fill text-center border-end py-2">
+                            <i class="fa fa-map-user text-primary me-2"></i> 2 Pers
+                         </small>
+                      </div>
+                   </div>
+
+                </div>
+            </div>
         </div>
     </div>
 
