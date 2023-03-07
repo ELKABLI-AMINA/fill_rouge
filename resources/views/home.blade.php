584246c7fd279f65a78ad682ef76cf5104084ef3
Amina <amina.elkabli1@gmail.com>
2023-03-07 19:47:26 +0100

    add galerie to footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -170,7 +170,36 @@
                     <p class="mb-2"><i class="bi bi-geo-alt me-3"></i> xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
                     <p class="mb-2"><i class="bi bi-telephone-fill me-3"></i> xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
                     <p class="mb-2"><i class="bi bi-envelope me-3"></i>info@example</p>
+                        {{-- social icon --}}
+                        <div class="d-flex pt-2">
+                            <a href="" class="btn btn-outline-light btn-social">
+                                <i class="bi bi-twitter"></i>
+                            </a>
+                            <a href="" class="btn btn-outline-light btn-social">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                            <a href="" class="btn btn-outline-light btn-social">
+                                <i class="bi bi-youtube"></i>
+                            </a>
+                            <a href="" class="btn btn-outline-light btn-social">
+                                <i class="bi bi-linkedin"></i>
+                            </a>
 
+                        </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Gallery</h4>
+                    <div class="row g-2 pt-2">
+                        <div class="col-4">
+                            <img src="{{asset('img/tanger.jpg')}}" alt="" class="img-fluid bg-light p-1">
+                        </div>
+                        <div class="col-4">
+                            <img src="{{asset('img/tanger.jpg')}}" alt="" class="img-fluid bg-light p-1">
+                        </div>
+                        <div class="col-4">
+                            <img src="{{asset('img/tanger.jpg')}}" alt="" class="img-fluid bg-light p-1">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
