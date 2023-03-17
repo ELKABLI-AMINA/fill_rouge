e47b6f68df3222059d26347f8955eb1ac08e7af4
amina <amina.elkabli1@gmail.com>
2023-03-17 20:18:56 +0100

    css carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+(function($){
+    "use strict"
+
+
+    $(".testimonial-coursel").owlCarousel({
+        autoplay: true,
+        smartSpeed:1000,
+        center :true,
+        margin:24,
+        dots:true,
+        loop:true,
+        nav:false,
+        Responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    })
+}
+)
