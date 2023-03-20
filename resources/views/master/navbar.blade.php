6eb2ab960d8f443680161b628e5e1a38f4c3c2dc
amina <amina.elkabli1@gmail.com>
2023-03-20 18:17:23 +0000

    finalisation du navbar

--- resources/views/master/navbar.blade.php
+++ resources/views/master/navbar.blade.php
@@ -1,32 +1,62 @@
 
-  <nav class="navbar navbar-expand-lg p-16 " >
-    <div class="container">
-      <a class="navbar-brand  " href="#"> <i class="bi bi-map"></i>Bladi</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="register">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact">Contact</a>
-          </li>
-         
-         
-        </ul>
-        
-      </div>
+<div class="header " style="width: 100%; height: 100vh; position:relative;">
+     <div class="nav_bar-top ">
+        <div class="container-fluid">
+                <div class="text-container d-flex align-items-center justify-content-between " >
+                    <div class="text-content d-flex justify-content-center">
+                      <p class="para-text "> <span class="span_text"> <i class="fa-solid fa-envelope"></i></span>Example@gmail.com</p>
+                      <p class="para-text "> <span class="span_text"> <i class="fa-solid fa-phone"></i></span>0522222222</p>
+                    </div>
+                        <div class="social-links">
+                          <i class="fa-brands icon-social fa-facebook"></i>
+                          <i class="fa-brands icon-social fa-snapchat"></i>
+                          <i class="fa-brands icon-social fa-whatsapp"></i>
+                          <i class="fa-brands icon-social fa-instagram"></i>
+                          <i class="fa-brands icon-social fa-linkedin-in"></i>
+                        </div>   
+                        <div class="navbar-container">
+                          <nav class="navbar navbar-expand-lg ">
+                              <a class="navbar-brand" href="#">
+                                <h3 class="heading-logo">BLA<span class="text-success">DI.</span></h3>
+                              </a>
+                              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                              </button>
+                                  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                    <ul class="navbar-nav ms-auto">
+                                      <li class="nav-item ">
+                                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                      </li>
+                                      <li class="nav-item">
+                                        <a class="nav-link" href="#">About</a>
+                                      </li>
+                                      <li class="nav-item">
+                                        <a class="nav-link" href="#">Contact</a>
+                                      </li>
+                                      <li class="nav-item">
+                                        <a class="nav-link" href="#">Packages</a>
+                                      </li>
+                                      <li class="nav-item">
+                                        <a class="nav-link" href="#">Register</a>
+                                      </li>
+                                      <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                          FAQ
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                          <a class="dropdown-item" href="#">About Us</a>
+                                          <a class="dropdown-item" href="#">Company</a>
+                                          <a class="dropdown-item" href="#">Experience</a>
+                                        </div>
+                                      </li>
+                                    </ul>
+                                </div>
+                          </nav>
+                    </div>         
+              </div>
+         </div>
     </div>
-  </nav>
+</div>
+
+
+ 
